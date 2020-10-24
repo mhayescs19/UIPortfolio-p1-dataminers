7d1ae526e997fe5027097b9135f612721801ddce
mhayescs19 <mhayescs19@gmail.com>
2020-10-23 22:46:54 -0700

    updated view testing functionality

--- src/view_control/HangmanUI.java
+++ src/view_control/HangmanUI.java
@@ -3,15 +3,25 @@ package view_control;
 import control_hangman.Hangman;
 import util.PrintyShortcuts;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.MatteBorder;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
+/**
+ * HANGMAN
+ * Current state
+ * 1. click guess tester to cycle through hangman states
+ * 2. click alphabet buttons to view console output of text (26 buttons created in a loop)
+ */
 
 public class HangmanUI extends JFrame {
-
+    
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             try {
@@ -21,6 +31,20 @@ public class HangmanUI extends JFrame {
                 e.printStackTrace();
             }
         });
+    }
+
+    private String[] hangmanImagePaths = new String[7];
+    int i = 1;
+
+    public void simulateGuess(JLabel image) {
+        hangmanImagePaths = new String[]{"src/images/hangman_initial.png", "src/images/hangman_guess1.png", "src/images/hangman_guess2.png", "src/images/hangman_guess3.png", "src/images/hangman_guess4.png", "src/images/hangman_guess5.png",  "src/images/hangman_full.png"};
+        if (i == 7) {
+            i = 0;
+        }
+        ImageIcon currentHangmanState = new ImageIcon(hangmanImagePaths[i]);
+        image.setIcon(currentHangmanState);
+        PrintyShortcuts.println(hangmanImagePaths[i]);
+        i += 1;
     }
 
     public HangmanUI() {
@@ -40,14 +64,15 @@ public class HangmanUI extends JFrame {
         int xCoordinate = 200; // starting x position of first alphabet button (a)
         int yCoordinate = 400; // starting y position of first alphabet button (a)
         int i = 0; // counter tracking number of buttons drawn on screen
-        /* For loop explanation
-         * 1. Cycle through entire alphabetButton array storing alphabet letters, creating a button for each letter
-         * 2. Use variable "letter" to set corresponding letter to each button
+        /** For loop explanation
+          * 1. Cycle through entire alphabetButton array storing alphabet letters, creating a button for each letter
+          * 2. Use variable "letter" to set corresponding letter to each button
+         * 3. Improvement = put buttons into an ArrayList since it is impossible to access attributes after creation
          */
         for (String letter : alphabetButtons) {
-            if (i == 12) { // after letter l, start a new line of the alphabet buttons
+            if (i == 13) { // after letter l, start a new line of the alphabet buttons
                 yCoordinate += 40;
-                xCoordinate = 160;
+                xCoordinate = 200; //160
             }
             JButton new_button = new JButton(letter.toUpperCase()); // text displayed on button
             new_button.addMouseListener(new MouseAdapter() {
@@ -66,11 +91,41 @@ public class HangmanUI extends JFrame {
             new_button.setForeground(Color.decode("#2B425B"));
             new_button.setFont(new Font("Gilroy-ExtraBold", Font.PLAIN, 25));
             new_button.setBackground(Color.WHITE);
-            new_button.addActionListener(e -> PrintyShortcuts.print(new_button.getText()));
-            new_button.setBounds(xCoordinate, yCoordinate, 30, 30);
+            new_button.addActionListener(e -> PrintyShortcuts.print(new_button.getText())); // test to check button values
+            new_button.setBounds(190 + xCoordinate, yCoordinate, 30, 30);
             hangmanFrame.add(new_button);
             xCoordinate += 40; // set x coordinate for next alphabet button (b) 40px to the right
             i++;
         }
+        /**
+         * Image Display Exploration
+         * 1. GOAL= practice switching icons of labels triggered by buttons
+         */
+        ImageIcon image = new ImageIcon("src/images/hangman_initial.png");
+        JLabel hangmanDisplay = new JLabel(image);
+        hangmanDisplay.setBounds(-10, -2, 400, 600);
+        hangmanFrame.add(hangmanDisplay);
+
+        JButton button_sim_guess = new JButton("guess tester"); // text displayed on button
+        button_sim_guess.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                button_sim_guess.setBackground(Color.decode("#9E9E9E"));
+            } // change color to get an interaction
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                button_sim_guess.setBackground(Color.WHITE);
+            }
+        });
+        button_sim_guess.setBorder(new MatteBorder(1, 1, 1, 1, Color.decode("#9DAF9B")));
+        button_sim_guess.setOpaque(true);
+        button_sim_guess.setForeground(Color.decode("#2B425B"));
+        button_sim_guess.setFont(new Font("Gilroy-ExtraBold", Font.PLAIN, 14));
+        button_sim_guess.setBackground(Color.WHITE);
+        button_sim_guess.addActionListener(e -> simulateGuess(hangmanDisplay)); // test to check button values
+        button_sim_guess.setBounds(600, 200, 100, 20);
+        hangmanFrame.add(button_sim_guess);
+
     }
 }
