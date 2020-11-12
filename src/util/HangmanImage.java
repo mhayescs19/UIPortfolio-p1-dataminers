ed8b91414030d7fbc91b46543b0119efec345bd6
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2020-11-11 20:54:51 -0800

    hangman changes

--- src/util/HangmanImage.java
+++ src/util/HangmanImage.java
@@ -5,6 +5,6 @@ public class HangmanImage {
     public static String[][] consoleArt;
 
     static {
-        filePath = new String[]{"src/images/hangman_initial.png", "src/images/hangman_guess1.png", "src/images/hangman_guess2.png", "src/images/hangman_guess3.png", "src/images/hangman_guess4.png", "src/images/hangman_guess5.png", "src/images/hangman_full.png"};
+        filePath = new String[]{"images/hangman_initial.png", "images/hangman_guess1.png", "images/hangman_guess2.png", "images/hangman_guess3.png", "images/hangman_guess4.png", "images/hangman_guess5.png", "images/hangman_full.png"};
     }
 }
